91e3ce1e376632ca02738b204284e1c752c00aa7
KaleoFeng <kaleofeng@gmail.com>
2019-11-22 18:17:03 +0800

    Piece buffer and ring buffer

---
+++ memory/piecebuffer.go
@@ -0,0 +1,115 @@
+package memory
+
+// PieceBuffer a piece of continuous memory
+type PieceBuffer struct {
+	buffer    []byte
+	pullIndex int
+	pushIndex int
+}
+
+// NewPieceBuffer returns a new piece buffer
+func NewPieceBuffer(capacity int) (pb *PieceBuffer) {
+	pb = &PieceBuffer{
+		make([]byte, capacity),
+		0,
+		0,
+	}
+	return
+}
+
+// Push push data into the buffer
+func (pb *PieceBuffer) Push(inData []byte) int {
+	pushLength := pb.GetPushLength()
+	if pushLength <= 0 {
+		return 0
+	}
+
+	inLength := len(inData)
+	if pushLength < inLength {
+		inLength = pushLength
+	}
+	copy(pb.buffer[pb.pushIndex:], inData)
+	pb.pushIndex += inLength
+	return inLength
+}
+
+// Pull pull data from the buffer
+func (pb *PieceBuffer) Pull(outBuffer []byte) int {
+	pullLength := pb.GetPullLength()
+	if pullLength <= 0 {
+		return 0
+	}
+
+	outLength := cap(outBuffer)
+	if pullLength < outLength {
+		outLength = pullLength
+	}
+	copy(outBuffer, pb.buffer[pb.pullIndex:pb.pullIndex+outLength])
+	pb.pullIndex += outLength
+	return outLength
+}
+
+// Peek peek data in the buffer
+func (pb *PieceBuffer) Peek(outBuffer []byte) int {
+	pullLength := pb.GetPullLength()
+	if pullLength <= 0 {
+		return 0
+	}
+
+	outLength := cap(outBuffer)
+	if pullLength < outLength {
+		outLength = pullLength
+	}
+	copy(outBuffer, pb.buffer[pb.pullIndex:pb.pullIndex+outLength])
+	return outLength
+}
+
+// Skip skip data of length in the buffer
+func (pb *PieceBuffer) Skip(length int) int {
+	pullLength := pb.GetPullLength()
+	if pullLength <= 0 {
+		return 0
+	}
+
+	if pullLength < length {
+		length = pullLength
+	}
+	pb.pullIndex += length
+	return length
+}
+
+// Compact compact the buffer
+func (pb *PieceBuffer) Compact() {
+	if pb.pullIndex == 0 {
+		return
+	}
+
+	if pb.pushIndex > pb.pullIndex {
+		copy(pb.buffer, pb.buffer[pb.pullIndex:pb.pushIndex])
+		pb.pushIndex = pb.pushIndex - pb.pullIndex
+		pb.pullIndex = 0
+	} else {
+		pb.pushIndex = 0
+		pb.pullIndex = 0
+	}
+}
+
+// GetBuffer get the buffer data
+func (pb *PieceBuffer) GetBuffer() []byte {
+	return pb.buffer
+}
+
+// GetCapacity get the buffer capacity
+func (pb *PieceBuffer) GetCapacity() int {
+	return cap(pb.buffer)
+}
+
+// GetPullLength get the buffer pull length
+func (pb *PieceBuffer) GetPullLength() int {
+	return pb.pushIndex - pb.pullIndex
+}
+
+// GetPushLength get the buffer push length
+func (pb *PieceBuffer) GetPushLength() int {
+	return pb.GetCapacity() - pb.pushIndex
+}
---
+++ memory/ringbuffer.go
@@ -0,0 +1,138 @@
+package memory
+
+// RingBuffer a ring circular buffer
+type RingBuffer struct {
+	buffer    []byte
+	length    int
+	pullIndex int
+	pushIndex int
+}
+
+// NewRingBuffer returns a new ring buffer
+func NewRingBuffer(capacity int) (pb *RingBuffer) {
+	pb = &RingBuffer{
+		make([]byte, capacity),
+		0,
+		0,
+		0,
+	}
+	return
+}
+
+// Push push data into the buffer
+func (rb *RingBuffer) Push(inData []byte) int {
+	inLength := len(inData)
+	vacant := rb.GetPushLength()
+	properLength := inLength
+	if vacant < inLength {
+		properLength = vacant
+	}
+	if properLength <= 0 {
+		return 0
+	}
+
+	rightLength := rb.GetCapacity() - rb.pushIndex
+	if properLength <= rightLength {
+		copy(rb.buffer[rb.pushIndex:], inData[:properLength])
+		rb.pushIndex += properLength
+	} else {
+		copy(rb.buffer[rb.pushIndex:], inData[:rightLength])
+		copy(rb.buffer, rb.buffer[rightLength:properLength])
+		rb.pushIndex = properLength - rightLength
+	}
+
+	rb.pushIndex %= rb.GetCapacity()
+	rb.length += properLength
+	return properLength
+}
+
+// Pull pull data from the buffer
+func (rb *RingBuffer) Pull(outBuffer []byte) int {
+	outLength := cap(outBuffer)
+	engaged := rb.GetPullLength()
+	properLength := outLength
+	if engaged < outLength {
+		properLength = engaged
+	}
+	if properLength <= 0 {
+		return 0
+	}
+
+	rightLength := rb.GetCapacity() - rb.pullIndex
+	if properLength <= rightLength {
+		copy(outBuffer, rb.buffer[rb.pullIndex:rb.pullIndex+properLength])
+		rb.pullIndex += properLength
+	} else {
+		copy(outBuffer, rb.buffer[rb.pullIndex:])
+		copy(outBuffer[rightLength:], rb.buffer[:properLength-rightLength])
+		rb.pullIndex = properLength - rightLength
+	}
+
+	rb.pullIndex %= rb.GetCapacity()
+	rb.length -= properLength
+	return properLength
+}
+
+// Peek peek data in the buffer
+func (rb *RingBuffer) Peek(outBuffer []byte) int {
+	outLength := cap(outBuffer)
+	engaged := rb.GetPullLength()
+	properLength := outLength
+	if engaged < outLength {
+		properLength = engaged
+	}
+	if properLength <= 0 {
+		return 0
+	}
+
+	rightLength := rb.GetCapacity() - rb.pullIndex
+	if properLength <= rightLength {
+		copy(outBuffer, rb.buffer[rb.pullIndex:rb.pullIndex+properLength])
+	} else {
+		copy(outBuffer, rb.buffer[rb.pullIndex:])
+		copy(outBuffer[rightLength:], rb.buffer[:properLength-rightLength])
+	}
+
+	return properLength
+}
+
+// Skip skip data of length in the buffer
+func (rb *RingBuffer) Skip(outLength int) int {
+	engaged := rb.GetPullLength()
+	properLength := outLength
+	if engaged < outLength {
+		properLength = engaged
+	}
+	if properLength <= 0 {
+		return 0
+	}
+
+	rb.pullIndex = (rb.pullIndex + properLength) % rb.GetCapacity()
+	rb.length -= properLength
+	return properLength
+}
+
+// IsEmpty check if the buffer is empty
+func (rb *RingBuffer) IsEmpty() bool {
+	return rb.length == 0
+}
+
+// IsFull check if the buffer is full
+func (rb *RingBuffer) IsFull() bool {
+	return rb.length == rb.GetCapacity()
+}
+
+// GetCapacity get the buffer capacity
+func (rb *RingBuffer) GetCapacity() int {
+	return cap(rb.buffer)
+}
+
+// GetPullLength get the buffer pull length
+func (rb *RingBuffer) GetPullLength() int {
+	return rb.length
+}
+
+// GetPushLength get the buffer push length
+func (rb *RingBuffer) GetPushLength() int {
+	return rb.GetCapacity() - rb.length
+}
